cc770904d1fb348a87862687bc89f0f134189fa1
Dravyk <dan.p.frye@gmail.com>
2026-01-12 22:45:20 -0600

    Update darknet-test.ts

--- darknet-test.ts
+++ darknet-test.ts
@@ -1,5 +1,6 @@
 export async function main(ns: NS) {
   ns.ui.clearTerminal();
+  console.clear();
   ns.scp("darknet-crawler.js", "darkweb", "home");
   ns.exec("darknet-crawler.js", "darkweb", { temporary: true });
 }
