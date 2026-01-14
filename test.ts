6e0a809e5cb020e5660ceef30b26a0b27225815d
Dravyk <dan.p.frye@gmail.com>
2026-01-13 23:09:14 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -7,37 +7,37 @@ export async function main(ns: NS) {
     passwordHint: `The password is a number between ${hintRange[0]} and ${hintRange[1]}`,
     passwordLength: pass.length
   };
-  const auth = (password: string): boolean => {
+  const authenticate = async (password: string) => {
+    await ns.sleep(200);
+    ++n;
     if (Number(password) > Number(pass)) details.data = "Lower";
     else if (Number(password) < Number(pass)) details.data = "Higher";
-    else return true;
-    return false;
+    else return { success: true };
+    return { success: false };
   };
+  let n = 0;
   ns.print(`AccountsManager_4.2 test`);
   /////////////////////////////////////////////////////////////////
 
   let [lowest, highest] = details.passwordHint.match(/\d+/g);
 
-  let i = 1;
   let result;
   let password;
   let guess = Math.round((highest - lowest) / 2);
   while (true) {
     password = `${guess}`.padStart(details.passwordLength, '0');
-    result = auth(password);
-    if (result) break;
+    result = await authenticate(password);
+    if (result.success) break;
     if (details.data === "Higher") {
       lowest = guess;
-      guess += Math.ceil((highest - guess) / 2);
+      guess += Math.floor((highest - guess) / 2);
     }
     else { // details.data = Lower
       highest = guess;
       guess -= Math.floor((guess - lowest) / 2);
     }
-    await ns.sleep(1e3);
-    ++i;
   }
 
   //////////////////////////////////////////////////////////////////
-  ns.print(`\npassword = ${password} in ${i} guesses`);
+  ns.print(`\npassword = ${password} in ${n} attempts`);
 }
