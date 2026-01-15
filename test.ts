19c896b72ce433e4b378c502aee99641cf8bb6a3
Dravyk <dan.p.frye@gmail.com>
2026-01-14 23:19:33 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,7 @@
 export async function main(ns: NS) {
+  ns.disableLog("ALL");
   ns.ui.openTail();
+  ns.clearLog();
   const pass = "309";
   const hintRange = [0, 1000];
   let details = {
@@ -7,12 +9,13 @@ export async function main(ns: NS) {
     passwordHint: `The password is a number between ${hintRange[0]} and ${hintRange[1]}`,
     passwordLength: pass.length
   };
-  const authenticate = async (password: string) => {
+  const authenticate = async (ns: NS, hostname: string, password: string) => {
     await ns.sleep(200);
-    ++n;
+    ns.print(`Attempt ${++n}: ${password}`);
     if (Number(password) > Number(pass)) details.data = "Lower";
     else if (Number(password) < Number(pass)) details.data = "Higher";
     else return { success: true };
+    ns.print(details.data);
     return { success: false };
   };
   let n = 0;
@@ -28,13 +31,13 @@ export async function main(ns: NS) {
     password = `${guess}`.padStart(details.passwordLength, '0');
     result = await authenticate(password);
     if (result.success) break;
-    if (details.data === "Higher") {
-      lowest = guess;
-      guess += Math.floor((highest - guess) / 2);
-    }
-    else { // details.data = Lower
+    if (details.data === "Lower") {
       highest = guess;
-      guess -= Math.floor((guess - lowest) / 2);
+      guess -= Math.round((highest - lowest) / 2);
+    }
+    else { // details.data = Higher
+      lowest = guess;
+      guess += Math.round((highest - lowest) / 2);
     }
   }
 
