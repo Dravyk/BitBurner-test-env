e56a4f1e96e246d9b66c184a161c24872a42401a
Dravyk <dan.p.frye@gmail.com>
2026-01-21 18:15:07 -0600

    Update radio.js

--- radio.js
+++ radio.js
@@ -36,7 +36,8 @@ export async function main(ns) {
     React.createElement("font",
       { color: `${ns.ui.getTheme()["cha"]}` },
       React.createElement("span", { class: "controls" },
-        React.createElement('a', { id: "play-btn",
+        React.createElement('a', {
+          id: "play-btn",
           onClick: () => {
             globalThis.musicPlaying = !globalThis.musicPlaying;
             doc.getElementById("radioplayer")[
@@ -128,38 +129,4 @@ export async function main(ns) {
 
   await ns.asleep(200);
   doc.getElementById("radioplayer").volume = 0.1;
-
-  // test button
-  ns.printRaw(React.createElement("div", { class: "test" },
-    React.createElement("style", { type: "text/css" }, `
-      .button {
-        border: 1px solid ${ns.ui.getTheme()["well"]};
-        color: ${ns.ui.getTheme()["primary"]};
-        padding: 8px 15px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-family: ${ns.ui.getStyles()["fontFamily"]};
-        font-size: ${ns.ui.getStyles()["tailFontSize"] - 1.5}px;
-        margin: 4px 2px;
-        transition-duration: 0.25s;
-        cursor: pointer;
-      }
-      .btn-test {
-        background-color: ${ns.ui.getTheme()["button"]};
-      }
-      .btn-test:hover {
-        background-color: ${ns.ui.getTheme()["backgroundsecondary"]}; 
-      }
-    `),
-    React.createElement("span", { class: "ikuhg" },
-      React.createElement("button", {
-        class: "button btn-test", type: "button",
-        onclick: `() => {
-          doc.getElementById("play-btn").innerText = "[Pause]";
-        }`,
-      }, "Play",
-      ),
-    ),
-  ));
 }
