0f4a72e8901b3f3c2f4d7ed17e0b5c9daa0500a8
Dravyk <dan.p.frye@gmail.com>
2026-01-31 21:55:33 -0600

    Delete darknet-test.ts

--- darknet-test.ts
+++
@@ -1,7 +0,0 @@
-export async function main(ns: NS) {
-  ns.ui.clearTerminal();
-  console.clear();
-  if (!ns.isRunning("overview.ts")) ns.run("overview.ts", { temporary: true });
-  ns.scp("darknet-crawler.js", "darkweb", "home");
-  ns.exec("darknet-crawler.js", "darkweb", { temporary: true });
-}
