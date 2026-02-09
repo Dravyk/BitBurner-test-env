ef6ea9138b1fd6a6a95aea2c4bcfdeee942a4c81
Dravyk <dan.p.frye@gmail.com>
2026-02-08 19:07:19 -0600

    Update overview.ts

--- overview.ts
+++ overview.ts
@@ -9,6 +9,9 @@ export async function main(ns: NS) {
   hook0.style = styleColor;
   hook1.style = styleColor;
 
+  // Grab current bitnode start time
+  const bnStart = ns.getResetInfo().lastNodeReset;
+
   // Callback to remove custom stats when script is killed
   ns.atExit(() => {
     hook0.innerHTML = "";
@@ -36,6 +39,9 @@ export async function main(ns: NS) {
       headers.push("Ram:");
       values.push(`${ns.format.ram(ns.getServerUsedRam("home"))}/${ns.format.ram(ns.getServerMaxRam("home"))}`);
 
+      headers.push("BitNode Time:");
+      values.push(formatTime(Date.now() - bnStart).padStart(12, '\xa0'));
+
       hook0.innerText = headers.join('\n');
       hook1.innerText = values.join('\n');
 
@@ -57,4 +63,51 @@ export async function main(ns: NS) {
 
     await ns.asleep(1e3);
   }
+}
+
+function formatTime(time: number): string {
+  time = Math.floor(time);
+  const millisecondsPerSecond = 1000;
+  const secondPerMinute = 60;
+  const minutesPerHours = 60;
+  const secondPerHours = secondPerMinute * minutesPerHours;
+  const hoursPerDays = 24;
+  const secondPerDay = secondPerHours * hoursPerDays;
+
+  // Convert ms to seconds, since we only have second-level precision
+  const totalSeconds = Math.floor(time / millisecondsPerSecond);
+
+  const days = Math.floor(totalSeconds / secondPerDay);
+  const secTruncDays = totalSeconds % secondPerDay;
+
+  const hours = `${Math.floor(secTruncDays / secondPerHours)}`;
+  const secTruncHours = secTruncDays % secondPerHours;
+
+  const minutes = `${Math.floor(secTruncHours / secondPerMinute)}`;
+
+  const seconds = `${secTruncHours % secondPerMinute}`;
+
+  let res = "";
+  let padHr = 1;
+  let padMin = 1;
+  let padSec = 1;
+
+  let showDay = days > 0;
+  let showHr = hours !== '0' || showDay;
+  let showMin = minutes !== '0' || showHr;
+
+  if (showDay) {
+    res += `${days}:`;
+    ++padHr;
+  }
+  if (showHr) {
+    res += `${hours.padStart(padHr, '0')}:`;
+    ++padMin;
+  }
+  if (showMin) {
+    res += `${minutes.padStart(padMin, '0')}:`;
+    ++padSec;
+  }
+
+  return res += `${seconds.padStart(padSec, '0')}`;
 }
